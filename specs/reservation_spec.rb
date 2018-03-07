a17ac89bb0b204e2cf8f13e5120183daa08f766c
Aruna <aruna.thungathurthi@gmail.com>
2018-03-07 12:19:54 -0800

    tests for reservaton class

---
+++ specs/reservation_spec.rb
@@ -0,0 +1,36 @@
+require_relative 'spec_helper'
+
+require 'date'
+
+describe "Reservation" do
+
+  describe "initialize" do
+    it "has a date range" do
+      admin = Hotel::Reservation.new("2018-9-01", "2018-9-04", 2)
+      admin.must_respond_to :checkin
+      admin.must_respond_to :checkout
+      admin.checkin.must_be_instance_of Date
+      admin.checkin.must_equal Date.new(2018, 9, 1)
+      admin.checkout.must_equal Date.new(2018, 9, 4)
+    end
+
+    it "has a room" do
+      admin = Hotel::Reservation.new("2018-9-01", "2018-9-04", 2)
+      admin.room.must_equal 2
+    end
+
+    it "has a cost" do
+      admin = Hotel::Reservation.new("2018-9-01", "2018-9-04", 2)
+      admin.must_respond_to :total_cost
+      admin.total_cost.must_equal 600
+    end
+
+    it "raises an error when given an incorrect date range" do
+      proc { Hotel::Reservation.new("2018-9-02", "2018-08-30", 3) }.must_raise ArgumentError
+
+      proc { Hotel::Reservation.new("2018-13-30", "2018-03-03", 3) }.must_raise ArgumentError
+
+      proc { Hotel::Resernation.new("2018-02-25", "2018-02-29")}
+    end
+  end
+end
---
+++ specs/spec_helper
@@ -0,0 +1,19 @@
+require 'simplecov'
+SimpleCov.start
+
+# specs/spec_helper.rb
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/pride'
+require 'minitest/skip_dsl'
+require 'pry'
+
+# Require any classes
+# ex require_relative 'lib/foo.rb'
+require_relative '../lib/hotel.rb'
+require_relative '../lib/room.rb'
+require_relative '../lib/reservation.rb'
+
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
