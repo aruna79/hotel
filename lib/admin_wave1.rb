0bb7bd217a0fa5ddf39d1c77ee49b2f8c492652d
Aruna <aruna.thungathurthi@gmail.com>
2018-03-10 18:33:02 -0800

    tests and code for reserve

--- lib/admin_wave1.rb
+++ lib/admin_wave1.rb
@@ -13,8 +13,6 @@ module Hotel
 
         @all_reservations = []
         @list_of_rooms = create_rooms
-
-
     end
 
      def create_rooms
@@ -42,7 +40,6 @@ module Hotel
       reservation = Hotel::Reservation.new(start_date,end_date,room_number)
       @all_reservations << reservation
       return @all_reservations
-
     end
 
     # - As an administrator, I can access the list of reservations for a specific date
@@ -68,12 +65,46 @@ module Hotel
         raise ArgumentError.new("invalid date")
       end
 
-      list = get_reservations(date)
+      reserved_list = get_reservations(date)
       reserved_rooms = []
-      list.each do |reservation|
+      reserved_list.each do |reservation|
         reserved_rooms << reservation.room_number
       end
       return @list_of_rooms - reserved_rooms
+      #return @available_rooms
+    end
+
+    # To reserve available rooms for a given date range
+    def reserve_available_rooms(start_date,end_date)
+
+      rooms_reserved_date = []
+      rooms_available_range = []
+      # to store list of all rooms
+      rooms_available_range = @list_of_rooms
+      # to find the rooms that are reserved for a given date in the range
+      (start_date...end_date).each do |date|
+      # to check which room is reserved for "date"
+        @all_reservations.each do |res|
+          list =[]
+          if res.start_date <= date && res.end_date > date
+            # Storing the reserved rooms in the "list" array
+            list << res.room_number
+          end
+          # remove the reserved rooms from all rooms
+          rooms_available_range = rooms_available_range - list
+        end
+      end
+
+      if rooms_available_range.empty?
+        raise Exception.new("No rooms are available")
+      else
+        availability = rooms_available_range[0]
+
+        reservation = Hotel::Reservation.new(start_date,end_date,availability)
+        @all_reservations << reservation
+        return @all_reservations
+      end
+
     end
 
     # - As an administrator, I can get the total cost for a given reservation
@@ -91,9 +122,31 @@ module Hotel
   end
 end
 
+
+
 # h= Hotel::Admin.new
-# start_date = Date.new(2018,5,3)
-# end_date =Date.new(2018,5,9)
-# puts h.make_reservation(start_date,end_date,3)
-# #puts h.total_cost(start_date,end_date)
-# puts h.list_reservations(Date.new(2018,5,4))
+#   start_date = Date.new(2018,5,3)
+#   end_date =Date.new(2018,5,6)
+#  #print "reservation of room\n"
+#  h.make_reservation(start_date,end_date)
+# # #puts h.total_cost(start_date,end_date)
+# # puts h.list_reservations(Date.new(2018,5,4))
+# #print"\n**************************************\n"
+# #res_room = []
+# #print "available room\n"
+# #h.list_of_available_rooms(Date.new(2018,5,6))
+# print"\n**************************************\n"
+# print "reserve available room for dates 5th to 7th\n"
+# 20.times do
+# h.reserve_available_rooms(Date.new(2018,5,5), Date.new(2018,5,7))
+# print"\n**************************************\n"
+# print "reserve available room for dates 5th to 7th\n"
+#
+# h.reserve_available_rooms(Date.new(2018,5,6), Date.new(2018,5,9))
+# print"\n**************************************\n"
+# #print "\n*********\n"
+# end
+# #print"\n**************************************\n"
+# #print "available room\n"
+# #print h.list_of_available_rooms(Date.new(2018,5,6))
+# #print res_room
--- specs/admin_wave1_spec.rb
+++ specs/admin_wave1_spec.rb
@@ -9,112 +9,155 @@ describe "instantiation" do
     @hotel.must_be_instance_of Hotel::Admin
   end
 
-  describe "get_list" do
 
-    it "returns the list of all rooms" do
-      @hotel.get_list.must_be_instance_of Array
-    end
+  it "returns the list of all rooms" do
+    @hotel.get_list.must_be_instance_of Array
+  end
+end
+
+describe "make_reservation" do
+  before do
+    @hotel = Hotel::Admin.new
+    start_date = Date.new(2018,05,03)
+    end_date = Date.new(2018,05,06)
+    @res = @hotel.make_reservation(start_date,end_date)
   end
 
-  describe "make_reservation" do
-    before do
-      @hotel = Hotel::Admin.new
-      start_date = Date.new(2018,05,03)
-      end_date = Date.new(2018,05,06)
-      @res = @hotel.make_reservation(start_date,end_date)
-    end
-
-    it "returns the list of all reservations" do
-      @res.class.must_equal Array
-
-    end
-
-    it "raises an error if date range is invalid" do
-      hotel = Hotel::Admin.new
-      start_date = Date.new(2018,05,10)
-      end_date =Date.new(2018,05,06)
-      proc{hotel.make_reservation(start_date,end_date)}.must_raise ArgumentError
-    end
+  it "returns the list of all reservations" do
+    @res.class.must_equal Array
 
   end
 
-  describe "get_reservations on particular date" do
+  it "raises an error if date range is invalid" do
+    hotel = Hotel::Admin.new
+    start_date = Date.new(2018,05,10)
+    end_date =Date.new(2018,05,06)
+    proc{hotel.make_reservation(start_date,end_date)}.must_raise ArgumentError
+  end
 
-    it "raises error if given date is invalid" do
-      hotel = Hotel::Admin.new
-      proc{hotel.get_reservations(2)}.must_raise ArgumentError
+end
 
-    end
+describe "get_reservations on particular date" do
 
-    it "returns the list of reservation available on a date" do
-      hotel = Hotel::Admin.new
-      hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,7))
-      hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,8))
-      hotel.make_reservation(Date.new(2018,05,7),Date.new(2018,05,9))
-
-      re = hotel.get_reservations(Date.new(2018,05,5))
-      re.length.must_equal 1
-    end
-
-    it "returns empty array if no reservation exists on that date" do
-      hotel = Hotel::Admin.new
-      hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,7))
-      hotel.make_reservation(Date.new(2018,05,6),Date.new(2018,05,8))
-      hotel.make_reservation(Date.new(2018,05,7),Date.new(2018,05,9))
-
-      reservations = hotel.get_reservations(Date.new(2018, 3, 11))
-      reservations.length.must_equal 0
-    end
+  it "raises error if given date is invalid" do
+    hotel = Hotel::Admin.new
+    proc{hotel.get_reservations(2)}.must_raise ArgumentError
 
   end
 
-  describe "total cost" do
+  it "returns the list of reservation available on a date" do
+    hotel = Hotel::Admin.new
+    hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,7))
+    hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,8))
+    hotel.make_reservation(Date.new(2018,05,7),Date.new(2018,05,9))
 
-    it "it access the total cost per night" do
-      hotel = Hotel::Admin.new
-      start_date = Date.new(2018,05,03)
-      end_date = Date.new(2018,05,06)
-      hotel.make_reservation(start_date,end_date)
+    re = hotel.get_reservations(Date.new(2018,05,5))
+    re.length.must_equal 1
+  end
 
-      hotel.must_respond_to :total_cost
-      cost = hotel.total_cost(start_date,end_date)
-      cost.must_be_kind_of Integer
-      cost.must_equal 600
+  it "returns empty array if no reservation exists on that date" do
+    hotel = Hotel::Admin.new
+    hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,7))
+    hotel.make_reservation(Date.new(2018,05,6),Date.new(2018,05,8))
+    hotel.make_reservation(Date.new(2018,05,7),Date.new(2018,05,9))
+
+    reservations = hotel.get_reservations(Date.new(2018, 3, 11))
+    reservations.length.must_equal 0
+  end
+
+end
+
+describe "total cost" do
+
+  it "it access the total cost per night" do
+    hotel = Hotel::Admin.new
+    start_date = Date.new(2018,05,03)
+    end_date = Date.new(2018,05,06)
+    hotel.make_reservation(start_date,end_date)
+
+    hotel.must_respond_to :total_cost
+    cost = hotel.total_cost(start_date,end_date)
+    cost.must_be_kind_of Integer
+    cost.must_equal 600
+
+  end
+
+
+  it "raises error for invalid dates" do
+    hotel = Hotel::Admin.new
+    start_date = Date.new(2018,05,9)
+    end_date = Date.new(2018,05,06)
+    #res = hotel.make_reservation(start_date,end_date,2)
+    proc{hotel.total_cost(start_date,end_date)}.must_raise ArgumentError
+
+
+  end
+end
+
+describe "list_of_available_rooms" do
+  it "raises error if given date is invalid" do
+    hotel = Hotel::Admin.new
+    proc{hotel.get_reservations(2)}.must_raise ArgumentError
+
+  end
+
+  it "returns the list of empty rooms available on a date" do
+    hotel = Hotel::Admin.new
+
+    hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,7))
+    hotel.make_reservation(Date.new(2018,05,6),Date.new(2018,05,8))
+    hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,9))
+
+
+    hotel.list_of_available_rooms(Date.new(2018,05,4)).length.must_equal 20
+    hotel.list_of_available_rooms(Date.new(2018,05,5)).length.must_equal 19
+    hotel.list_of_available_rooms(Date.new(2018,05,6)).length.must_equal 19
+    hotel.list_of_available_rooms(Date.new(2018,05,8)).length.must_equal 20
+
+  end
+
+end
+
+describe "reserve_list_available_rooms" do
+
+  it "returns the list of all reservations" do
+    hotel = Hotel::Admin.new
+    start_date = Date.new(2018,05,03)
+    end_date = Date.new(2018,05,06)
+    reservation = hotel.reserve_available_rooms(start_date,end_date)
+    reservation.class.must_equal Array
+
+  end
+
+  it "returns the newly created reservation" do
+    hotel = Hotel::Admin.new
+    start_date = Date.new(2018,05,03)
+    end_date = Date.new(2018,05,06)
+    before_length = hotel.all_reservations.length
+    reservation = hotel.reserve_available_rooms(start_date,end_date)
+    reservation.length.must_equal before_length + 1
 
     end
 
-    it"raisese error for invalid dates" do
-      hotel = Hotel::Admin.new
-      start_date = Date.new(2018,05,9)
-      end_date = Date.new(2018,05,06)
-      #res = hotel.make_reservation(start_date,end_date,2)
-      proc{hotel.total_cost(start_date,end_date)}.must_raise ArgumentError
-
-
-    end
-
-    describe "list_of_available_rooms" do
-      it "raises error if given date is invalid" do
-        hotel = Hotel::Admin.new
-        proc{hotel.get_reservations(2)}.must_raise ArgumentError
-
+  it "raises an error there are no available rooms" do
+    hotel = Hotel::Admin.new
+      hotel.list_of_available_rooms(Date.new(2018,5,3))
+      20.times do
+        hotel.reserve_available_rooms(Date.new(2018,5,3),Date.new(2018,5,6))
       end
 
-      it "returns the list of empty rooms available on a date" do
-        hotel = Hotel::Admin.new
-
-        hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,7))
-        hotel.make_reservation(Date.new(2018,05,6),Date.new(2018,05,8))
-        hotel.make_reservation(Date.new(2018,05,5),Date.new(2018,05,9))
-
-
-        hotel.list_of_available_rooms(Date.new(2018,05,4)).length.must_equal 20
-        hotel.list_of_available_rooms(Date.new(2018,05,5)).length.must_equal 19
-        hotel.list_of_available_rooms(Date.new(2018,05,6)).length.must_equal 19
-        hotel.list_of_available_rooms(Date.new(2018,05,8)).length.must_equal 20
-
-      end
-
-    end
+    proc{hotel.reserve_available_rooms(Date.new(2018,5,3),Date.new(2018,5,6))}.must_raise Exception
   end
+
+
+  it "raises an error for invalid date range" do
+    hotel = Hotel::Admin.new
+    start_date = Date.new(2018,5,10)
+    end_date =Date.new(2018,5,6)
+    proc{hotel.reserve_available_rooms(start_date,end_date)}.must_raise ArgumentError
+
+  end
+
+
+
 end
