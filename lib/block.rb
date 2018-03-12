ae0319451bc030a77d9284c3df27b14b1b2f0390
Aruna <aruna.thungathurthi@gmail.com>
2018-03-11 17:53:20 -0700

    bockreservation class

--- lib/block.rb
+++ lib/block.rb
@@ -1,12 +1,13 @@
 module Hotel
   class BlockReservation
-    attr_reader :start_date,:end_date,:room_number, :discount_rate, :block_id
-    def initialize(start_date,end_date,room_number,discount_rate,block_id)
+    attr_reader :start_date,:end_date,:room_number, :discount_rate, :block_id, :status
+    def initialize(start_date,end_date,room_number,discount_rate,block_id, status=true)
       @start_date = start_date
       @end_date = end_date
       @room_number = room_number
       @discount_rate = discount_rate
       @block_id = block_id
+      @status = status
     end
 
 
