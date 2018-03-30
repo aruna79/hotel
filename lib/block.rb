9c7b1628b21444a1a631a43ae84382f45881c19a
Aruna <aruna.thungathurthi@gmail.com>
2018-03-30 12:03:56 -0700

    added methods in block class

--- lib/block.rb
+++ lib/block.rb
@@ -9,6 +9,29 @@ module Hotel
       @block_id = block_id
       @status = status
     end
+    def create_block(start_date, end_date, block_id, discount_rate)
+      if start_date.class != Date || end_date.class!=Date
+        raise ArgumentError.new("Invalid date")
+      end
+      avail_rooms = get_available_rooms(start_date,end_date)
+
+      (0..4).each do |i|
+        room_block = avail_rooms[i]
+        #  print room_block
+        reservation_block = Hotel::BlockReservation.new(start_date,end_date,room_block, discount_rate, block_id)
+        @block_reservation << reservation_block
+      end
+      return @block_reservation
+
+    end
+    def check_block_availability(block_id)
+      block_records = @block_reservation.select{|block| block.block_id == block_id && block.status == true}
+      if block_records.empty?
+        raise Exception.new("No rooms are available in the block")
+      else
+        return block_records
+      end
+    end
 
 
   end
