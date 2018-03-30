bbd3777766af8f4eeed12fbdf2eada354aea18a9
Aruna <aruna.thungathurthi@gmail.com>
2018-03-30 12:05:24 -0700

    moved list_of rooms method from admin to room class

--- lib/room_wave1.rb
+++ lib/room_wave1.rb
@@ -9,6 +9,19 @@ module Hotel
         raise ArgumentError.new("Invalid room number")
       end
     end
+    def list_of_available_rooms(date)
+      if date.class!= Date
+        raise ArgumentError.new("invalid date")
+      end
+
+      reserved_list = get_reservations(date)
+      reserved_rooms = []
+      reserved_list.each do |reservation|
+        reserved_rooms << reservation.room_number
+      end
+      return @list_of_rooms - reserved_rooms
+    end
+
 
   end
 end
